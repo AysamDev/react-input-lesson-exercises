1b79966862c72f4f304b792b87bffa981a66d7c7
Aysam Salalha <aysam.s.1998@hotmail.com>
2020-12-24 18:48:40 +0200

    done with react input lesson

--- src/components/Exercise1.js
+++ src/components/Exercise1.js
@@ -9,13 +9,27 @@ class Exercise1 extends Component {
             age: ""
         }
     }
-
+    handleInput = (event) =>
+    {
+        const target = event.target;
+        let name = target.id === 'name-input'?'name':'age'
+        let value = target.value
+        this.setState(
+            {
+                [name]: value
+            }
+        )
+    }
+    alertBar = () =>
+    {
+        alert(`Come in ${this.state.name}, you're ${this.state.age} - that's good enough`)
+    }
     render() {
         return (
             <div>
-                <input id="name-input" />
-                <input id="age-input" />
-                <button>Go to Bar</button>
+                <input id="name-input" value={this.state.name} onChange={this.handleInput}/>
+                <input id="age-input" value={this.state.age} onChange={this.handleInput}/>
+                <button onClick={this.alertBar}>Go to Bar</button>
             </div>
         );
     }
--- src/components/Exercise2.js
+++ src/components/Exercise2.js
@@ -10,11 +10,31 @@ constructor() {
     }
 }
 
+handleInput = (event) =>
+{
+    const target = event.target;
+    let name = target.id === 'name-input'?'name':'fruit'
+    let value = target.value
+    this.setState(
+        {
+            [name]: value
+        },
+        function()
+        {
+            console.log(`${this.state.name} selected ${this.state.fruit}`)
+        }
+    )
+}
 render() {
     return (
         <div>
-            <input id="name-input" />
-            <select id="select-input"></select>
+            <input id="name-input" value={this.state.name} onChange={this.handleInput}/>
+            <select id="select-input" value={this.state.fruit} onChange={this.handleInput}>
+            <option value="Strawberry">Strawberry</option>
+            <option value="Banana">Banana</option>
+            <option value="Apple">Apple</option>
+            </select>
+          
         </div>
     );
 }
